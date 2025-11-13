984e875ffc0fefd712dd20e418ccf7c0763a61a2
DarkEden-coding <darkedenc9@gmail.com>
2025-11-12 23:11:06 -0500

    add FlatpackDecoder import, refactor periodic method to use Optional, update target pose decoding to use raw packets, add translation extraction for Vector3 and Vector2 arrays, add schema label determination for logging

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -6,6 +6,7 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.FlatpackDecoder;
 import java.util.Optional;
 import org.littletonrobotics.junction.Logger;
 
@@ -18,10 +19,8 @@ public class Vision extends SubsystemBase {
 
   @Override
   public void periodic() {
-    Logger.recordOutput("EagleEye/HasTarget", getTargetPose().isPresent());
-    if (getTargetPose().isPresent()) {
-      Logger.recordOutput("EagleEye/TargetPose", getTargetPose().get());
-    }
+    Optional<Pose3d> targetPose = getTargetPose();
+    targetPose.ifPresent(pose -> Logger.recordOutput("EagleEye/TargetPose", pose));
   }
 
   /**
@@ -31,25 +30,66 @@ public class Vision extends SubsystemBase {
    */
   public Optional<Pose3d> getTargetPose() {
     var positionEntry = visionTable.getEntry(VisionConstants.kTargetPositionEntryName);
-    double[] positionArray = positionEntry.getDoubleArray(new double[0]);
+    byte[] rawPacket = positionEntry.getRaw(new byte[0]);
 
-    if (positionArray.length >= 3) {
-      // Convert Vector3 (x, y, z) to Pose3d with identity rotation
-      Translation3d translation =
-          new Translation3d(positionArray[0], positionArray[1], positionArray[2]);
-      Rotation3d rotation = new Rotation3d(); // Identity rotation
-      Pose3d targetPose = new Pose3d(translation, rotation);
-
-      // Log vision data
-      Logger.recordOutput("EagleEye/HasTarget", true);
-      Logger.recordOutput("EagleEye/TargetPose", targetPose);
-      Logger.recordOutput("EagleEye/TargetPosition", translation);
-
-      return Optional.of(targetPose);
-    } else {
-      // No valid data available
+    if (rawPacket.length == 0) {
       Logger.recordOutput("EagleEye/HasTarget", false);
       return Optional.empty();
     }
+
+    try {
+      Object decodedPayload = FlatpackDecoder.decode(rawPacket);
+      Logger.recordOutput("EagleEye/FlatpackSchema", determineSchemaLabel(decodedPayload));
+      Optional<Translation3d> translation = extractTranslation(decodedPayload);
+
+      if (translation.isPresent()) {
+        Pose3d targetPose = new Pose3d(translation.get(), new Rotation3d());
+        Logger.recordOutput("EagleEye/HasTarget", true);
+        Logger.recordOutput("EagleEye/TargetPosition", translation.get());
+        return Optional.of(targetPose);
+      }
+
+      Logger.recordOutput("EagleEye/HasTarget", false);
+      Logger.recordOutput("EagleEye/FlatpackError", "Unsupported payload structure");
+      return Optional.empty();
+    } catch (IllegalArgumentException exception) {
+      Logger.recordOutput("EagleEye/HasTarget", false);
+      Logger.recordOutput("EagleEye/FlatpackError", exception.getMessage());
+      return Optional.empty();
+    }
+  }
+
+  private Optional<Translation3d> extractTranslation(Object decodedPayload) {
+    if (decodedPayload instanceof FlatpackDecoder.Vector3[] vectors && vectors.length > 0) {
+      FlatpackDecoder.Vector3 vector = vectors[0];
+      return Optional.of(new Translation3d(vector.x, vector.y, vector.z));
+    }
+
+    if (decodedPayload instanceof FlatpackDecoder.Vector2[] vectors && vectors.length > 0) {
+      FlatpackDecoder.Vector2 vector = vectors[0];
+      return Optional.of(new Translation3d(vector.x, vector.y, 0.0));
+    }
+
+    if (decodedPayload instanceof float[] values && values.length >= 3) {
+      return Optional.of(new Translation3d(values[0], values[1], values[2]));
+    }
+
+    return Optional.empty();
+  }
+
+  private String determineSchemaLabel(Object decodedPayload) {
+    if (decodedPayload instanceof FlatpackDecoder.Vector3[]) {
+      return "vector3_array";
+    }
+
+    if (decodedPayload instanceof FlatpackDecoder.Vector2[]) {
+      return "vector2_array";
+    }
+
+    if (decodedPayload instanceof float[]) {
+      return "float_array";
+    }
+
+    return decodedPayload.getClass().getSimpleName();
   }
 }

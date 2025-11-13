3cd32a043eed4e33cc9d9bd38aaf1819ac27cccc
DarkEden-coding <darkedenc9@gmail.com>
2025-11-12 23:11:14 -0500

    update build constants with new revision info, update maven name, update git metadata

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "wapur-2025-migrated";
+  public static final String MAVEN_NAME = "Wapur-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "41258b2d97d937dff82e72b710f228924afcc39e";
-  public static final String GIT_DATE = "2025-11-12 21:52:32 EST";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "ee7e6270b44bcf58715db350308cd55d736d419d";
+  public static final String GIT_DATE = "2025-11-12 21:59:39 EST";
   public static final String GIT_BRANCH = "Elliot-Backup-Code";
-  public static final String BUILD_DATE = "2025-11-12 21:59:22 EST";
-  public static final long BUILD_UNIX_TIME = 1763002762129L;
+  public static final String BUILD_DATE = "2025-11-12 23:06:10 EST";
+  public static final long BUILD_UNIX_TIME = 1763006770713L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

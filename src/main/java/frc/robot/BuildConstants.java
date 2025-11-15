170424f6db2b38127eae1209d84065f4e2548213
The_Pie <piemaster122@gmail.com>
2025-11-15 14:02:17 -0500

    Updated Elevator and added util folder/SparkUtil File

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Wapur-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "e20736e7d2efb27f680d4deb581c1e6c686738b3";
-  public static final String GIT_DATE = "2025-11-12 22:00:30 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "526f0202cb9fdba6a564e39a36cee289fe99732b";
+  public static final String GIT_DATE = "2025-11-13 20:45:21 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-11-13 19:41:04 EST";
-  public static final long BUILD_UNIX_TIME = 1763080864623L;
+  public static final String BUILD_DATE = "2025-11-15 14:00:11 EST";
+  public static final long BUILD_UNIX_TIME = 1763233211921L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

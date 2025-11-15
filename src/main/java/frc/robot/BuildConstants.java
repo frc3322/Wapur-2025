a1aa553a811172e5ec6d9a8f516a95525ac7401a
Egglord <evanr121309@gmail.com>
2025-11-15 18:34:12 -0500

    Added shooter fixed bugs with crate

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Wapur-2025";
+  public static final String MAVEN_NAME = "Wapur-2025-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "e20736e7d2efb27f680d4deb581c1e6c686738b3";
-  public static final String GIT_DATE = "2025-11-12 22:00:30 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "526f0202cb9fdba6a564e39a36cee289fe99732b";
+  public static final String GIT_DATE = "2025-11-13 20:45:21 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-11-13 19:41:04 EST";
-  public static final long BUILD_UNIX_TIME = 1763080864623L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-11-15 12:12:37 EST";
+  public static final long BUILD_UNIX_TIME = 1763226757357L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIO.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.Shooter;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ShooterIO {
+    
+    @AutoLog
+    public static class ShooterIOinputs {
+        
+    }
+
+}
